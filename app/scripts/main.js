d4e39e5d664f43fe8270dbae239aed720266e60a
Sunny Mui <sunnycmui@gmail.com>
2017-08-04 14:20:18 -0700

    refactor image processing into a separate web worker thread, use request animation frame instead of set interval

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -28,14 +28,14 @@
 
     cameraManager.onframe = function() {
       // There is a frame in the camera, what should we do with it?
- 
+
       var imageData = cameraManager.getImageData();
       var detectedQRCode = qrCodeManager.detectQRCode(imageData, function(url) {
         if(url !== undefined) {
           qrCodeManager.showDialog(url);
         }
       });
-    
+
     };
   };
 
@@ -48,6 +48,9 @@
 
     var client = new QRClient();
 
+    // spawn a web worker for qr code decoding
+    var qr_worker = new Worker('/scripts/jsqrcode/qrworker.js');
+
     var self = this;
 
     this.currentUrl = undefined;
@@ -56,12 +59,23 @@
     this.detectQRCode = function(imageData, callback) {
       callback = callback || function() {};
 
-      client.decode(imageData, function(result) {
-        if(result !== undefined) {
+      qr_worker.addEventListener('message', function(e){
+        var result = e.data;
+        if (result !== undefined) {
           self.currentUrl = result;
         }
         callback(result);
       });
+
+      // post image data to worker here for decoding
+      qr_worker.postMessage(imageData);
+
+      // client.decode(imageData, function(result) {
+      //   if(result !== undefined) {
+      //     self.currentUrl = result;
+      //   }
+      //   callback(result);
+      // });
     };
 
     this.showDialog = function(url) {
@@ -105,7 +119,7 @@
 
     // Variables
     var dWidth;
-    var dHeight; 
+    var dHeight;
     var dx = 0;
     var dy = 0;
 
@@ -150,19 +164,19 @@
         overlayCoords.height = cameraCanvas.height - (boxHeightSize * 2);
 
       }
-     
+
     };
 
     var setupVariables = function(e) {
       dWidth = cameraCanvas.width = window.innerWidth;
-      dHeight = cameraCanvas.height = window.innerHeight; 
+      dHeight = cameraCanvas.height = window.innerHeight;
       dx = 0;
       dy = 0;
 
       sx = 0;
       sy = 0;
 
-      // Make the video coordinate space the same as the window. 
+      // Make the video coordinate space the same as the window.
       // size in the longest dimension.
       // Then center and clip. and map back to correct space.
       scaleX = (dWidth / cameraVideo.videoWidth);
@@ -172,7 +186,7 @@
       // Trim the left
       sx = ((cameraVideo.videoWidth * scaleFactor) / 2) - (dWidth/ 2);
       sy = ((cameraVideo.videoHeight * scaleFactor) / 2) - (dHeight / 2);
-     
+
       // Trim the right.
       sWidth = (cameraVideo.videoWidth * scaleFactor) - sx * 2;
       sHeight = (cameraVideo.videoHeight * scaleFactor) - sy * 2;
@@ -191,6 +205,7 @@
       if(self.onframe) self.onframe();
 
       coordinatesHaveChanged = false;
+      requestAnimationFrame(captureFrame);
     };
 
     var getCamera = function(videoSource, cb) {
@@ -206,37 +221,36 @@
 
       if(videoSource === undefined && cameras.length == 0) {
         // Because we have no source information, have to assume it user facing.
-        params = { video: true }; 
+        params = { video: true };
       }
       else {
         params = { video: { optional: [{sourceId: videoSource.id}] } };
       }
-  
+
       gUM.call(navigator, params, function(theStream) {
         localStream = theStream;
-        
+
         cameraVideo.onloadeddata = function(e) {
 
           coordinatesHaveChanged = true;
-          
+
           var isSetup = setupVariables(e);
           if(isSetup) {
-            setInterval(captureFrame.bind(self), 4);
+            requestAnimationFrame(captureFrame.bind(self));
           }
           else {
             // This is just to get around the fact that the videoWidth is not
             // available in Firefox until sometime after the data has loaded.
             setTimeout(function() {
               setupVariables(e);
-
-              setInterval(captureFrame.bind(self), 4);
+              requestAnimationFrame(captureFrame.bind(self));
             }, 100);
           }
 
           // The video is ready, and the camerea captured
           if(videoSource === undefined) {
             // There is no meta data about the camera, assume user facing.
-            videoSource = { 
+            videoSource = {
               'facing': 'user'
             };
           }
@@ -327,7 +341,7 @@
     });
 
     // Init
-    getSources(function() { 
+    getSources(function() {
       // On first run, select the first camera.
       getCamera(cameras[0], toggleFacingState);
     });
